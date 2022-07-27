6a8f0f4510c3fb32c40586ca417bcb6a0e2ee2cb
Choi Soobin <sbchoi9641@gmail.com>
2022-07-27 04:35:07 +0000

    modified:   Order/kubernetes/deployment.yaml 	modified:   Order/src/main/java/hanwhadeliverysystemteam/infra/OrderController.java

--- Order/src/main/java/hanwhadeliverysystemteam/infra/OrderController.java
+++ Order/src/main/java/hanwhadeliverysystemteam/infra/OrderController.java
@@ -39,4 +39,29 @@ public class OrderController {
         orderRepository.delete(order);
         return order;
     }
+
+    // liveness probe 테스트 
+    @GetMapping("/callMemleak")
+    public void callMemleak() {
+        try{
+            this.memLeak();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void memLeak() throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
+        Class unsafeClass = Class.forName("sun.misc.Unsafe");
+        Field f = unsafeClass.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        Unsafe unsafe = (Unsafe) f.get(null);
+        System.out.print("4..3..2..1...");
+        try
+        {
+            for(;;)
+            unsafe.allocateMemory(1024*1024);
+        } catch(Error e) {
+            System.out.println("Boom :)");
+            e.printStackTrace();
+        }
+    }
 }
